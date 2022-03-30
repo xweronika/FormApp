15af66b75541b6ad03181032897bfd9d2d6d2046
xweronika <xweronika19@gmail.com>
2022-03-30 14:52:38 +0200

    Init form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { FormControlComponent } from './form-control/form-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FormControlComponent
   ],
   imports: [
     BrowserModule
