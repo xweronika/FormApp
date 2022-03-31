5320d861ad86c05090fd685cdb0ad8ab41a95809
xweronika <xweronika19@gmail.com>
2022-03-31 21:09:28 +0200

    Update products data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Product } from 'src/app/interfaces/product';
 
@@ -11,7 +11,13 @@ export class AppComponent {
   public open: boolean = false;
   public title: string = "Wybór produktów";
   public form: FormGroup;
-  public products: Array<Product> = [{ id: 'Bananas', name: '30', description: 'in stock' }, { id: 'Apples', name: '42', description: 'N/A' }, { id: 'Appleas', name: '422', description: 'N/As' }];
+  public products: Array<Product> =
+    [{ id: 1, name: 'Produkt 1', description: 'Produkt 1 - najlepszy' },
+    { id: 2, name: 'Produkt 2', description: 'Produkt 2 - najlepszy' },
+    { id: 3, name: 'Produkt 3', description: 'Produkt 3 - najlepszy' },
+    { id: 4, name: 'Produkt 4', description: 'Produkt 4 - najlepszy' },
+    { id: 5, name: 'Produkt 5', description: 'Produkt 5 - najlepszy' },
+    { id: 6, name: 'Produkt 6', description: 'Produkt 6 - najlepszy' }];
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({ product: '' });
